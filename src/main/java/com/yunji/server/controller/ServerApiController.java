32ede29cca006948f0605627b18ed0146f950c11
yj.ko <yunjigo92@gmail.com>
2022-05-12 14:46:43 +0900

    add 간단한 네이버 api 연동 예제

--- src/main/java/com/yunji/server/controller/ServerApiController.java
+++ src/main/java/com/yunji/server/controller/ServerApiController.java
@@ -3,12 +3,54 @@ package com.yunji.server.controller;
 import com.yunji.server.dto.Req;
 import com.yunji.server.dto.User;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
 
 @Slf4j
 @RestController
 @RequestMapping("/api/server")
 public class ServerApiController {
+
+
+    @GetMapping("/naver")
+    public String naver(){
+
+
+        String query = "갈비집";
+
+        //https://openapi.naver.com/v1/search/local.json?query=%EA%B0%88%EB%B9%84%EC%A7%91%0A&display=10&start=1&sort=random
+        URI uri = UriComponentsBuilder
+                .fromUriString("https://openapi.naver.com")
+                .path("/v1/search/local.json")
+                .queryParam("query",query)
+                .queryParam("display",10)
+                .queryParam("start",1)
+                .queryParam("sort","random")
+                .encode(Charset.forName("UTF-8"))
+                .build()
+                .toUri();
+
+        RequestEntity<Void> req = RequestEntity
+                .get(uri)
+                .header("X-Naver-Client-Id","aRyjDlNoJdgzg9rMHKtq")
+                .header("X-Naver-Client-Secret","8EsKcxb_a4")
+                .build();
+
+        RestTemplate restTemplate = new RestTemplate();
+
+        log.info("log: {}",uri);
+        ResponseEntity<String> resoponseEntity = restTemplate.exchange(req, String.class);
+
+        return resoponseEntity.getBody();
+    }
 
 
     @GetMapping("/hello")
